bdfa586ce377d7dc97e3c06f0ed29d99e31638fa
Facundo <frank.f96@hotmail.com>
2019-02-28 02:29:25 +0100

    -

--- inf104/TP_SELC/TP13/material/TP13_ign_all.c
+++ inf104/TP_SELC/TP13/material/TP13_ign_all.c
@@ -2,16 +2,26 @@
 #include <signal.h>
 #include <unistd.h>
 int main(int argc, char *argv[]){
+
   int Nb_Sig;
+  int Val_Sig;
+
   printf("NSIG = %d", NSIG);
   sleep(3);
+
+
   for(Nb_Sig = 1; Nb_Sig < NSIG ; Nb_Sig ++)
   {
-   
-  } 
+   Val_Sig = signal(Nb_Sig , SIG_IGN);
+   printf("Valeur renvoyée pour : %d %d\n",Nb_Sig , Val_Sig );
+  }
+
+
   while(1)
   {
     sleep(5);
   } /* Wait for receiving signals */
+
+
   return 0;
 }
